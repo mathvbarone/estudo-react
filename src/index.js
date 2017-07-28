7841707326e51c71ee837a57ecaf4555709c46fb
Matheus Barone <matheusvbarone@gmail.com>
2017-07-28 14:32:59 -0300

    Criando o componente input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCbWEE2a4Pc-qfGYTJ6YjgYGuyKa2PFEGA';
 
 
 //CRIAR UM NOVO COMPONENTE. ESSE COMPONENDE DEVE PRODUZIR ALGUM HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return(
+        <div>
+            <SearchBar />
+        </div>
+    ); 
 }
 
 //PEGAR ESSE COMPONENTE HTML GERADO, E COLOCAR ISSO NA PÁGINA
