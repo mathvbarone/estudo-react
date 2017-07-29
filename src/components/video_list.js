441ed93cfa51836f1a0460a8af7b9dc368e879b5
Matheus Barone <matheusvbarone@gmail.com>
2017-07-29 17:36:37 -0300

    Deixando a lista de videos dinamica

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,6 +2,7 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+    const video = props.video;
     const videoItems = props.videos.map((video) => {
         return <VideoListItem key={video.etag} video={video} />
     });
