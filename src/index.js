596f4f81d47b127d8e2d31a91c07880c94f548eb
Matheus Barone <matheusvbarone@gmail.com>
2017-07-29 15:01:53 -0300

    Refatorando Componente funcionais para componente de classe

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCbWEE2a4Pc-qfGYTJ6YjgYGuyKa2PFEGA';
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-});
 
 
 //CRIAR UM NOVO COMPONENTE. ESSE COMPONENDE DEVE PRODUZIR ALGUM HTML
-const App = () => {
-    return(
-        <div>
-            <SearchBar />
-        </div>
-    ); 
+class App extends Component {
+    constructor (props) {
+        super (props);
+
+        this.state={ videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <SearchBar />
+            </div>
+        ); 
+    }
 }
 
 //PEGAR ESSE COMPONENTE HTML GERADO, E COLOCAR ISSO NA PÁGINA
