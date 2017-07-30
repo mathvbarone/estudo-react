4e31971755860d12d1f96ec4a5ae9bbfce7939b7
Matheus Barone <matheusvbarone@gmail.com>
2017-07-30 12:41:24 -0300

    Habilitando a busca dos videos no input

--- src/index.js
+++ src/index.js
@@ -15,9 +15,12 @@ class App extends Component {
         this.state = { 
             videos: [],
             selectedVideo : null
-        };
+        };       
+        this.videoSearch('blink-182');
+    }
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) =>{
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0]
@@ -28,7 +31,7 @@ class App extends Component {
     render(){
         return(
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
